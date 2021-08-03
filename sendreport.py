b7d3d0d0056a8ca6d7a64052be8274b0991c4482
Pinto565 <pintoinfant5650@outlook.com>
2021-08-03 12:51:41 +0000

    Notification

--- sendreport.py
+++ sendreport.py
@@ -1,7 +1,7 @@
 import requests
 import os
 oid = os.environ['OWNER_ID']
-bot_token = os.environ['TELEGRAM_TOKEN']
+bot_token = os.environ['TELEGRAM2_TOKEN']
 def send__message(message):
   base_url = ("https://api.telegram.org/bot"+bot_token+"/sendMessage")
   parameters = {
